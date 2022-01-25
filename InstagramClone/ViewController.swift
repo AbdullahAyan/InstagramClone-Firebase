bf51dc2974efe3655b3286abfde92c5a5e68e095
AbdullahAyan <abdullahayanx@outlook.com>
2022-01-26 02:05:37 +0300

    Firebase Update
    Storyboard updated and Create User added

--- InstagramClone/ViewController.swift
+++ InstagramClone/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    @IBAction func signInClicked(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+        if emailText.text != "" && passwordText.text != "" {
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { authdata, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                }else{
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        }else{
+            makeAlert(titleInput: "Error", messageInput: "Username/Password is missing.")
+        }
+        
 
+        
+        
+    }
+    
+    func makeAlert(titleInput:String,messageInput:String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 

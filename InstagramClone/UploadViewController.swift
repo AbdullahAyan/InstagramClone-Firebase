0c856c52259e5dcfcd8cf1ac5155ef6fdabbc60f
AbdullahAyan <abdullahayanx@outlook.com>
2022-01-26 13:12:16 +0300

    Uploading

--- InstagramClone/UploadViewController.swift
+++ InstagramClone/UploadViewController.swift
@@ -6,26 +6,71 @@
 //
 
 import UIKit
+import Firebase
+import CoreMedia
 
-class UploadViewController: UIViewController {
-    
-    
-    
+class UploadViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+        
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var commentText: UITextField!
     @IBOutlet weak var uploadButton: UIButton!
     
     
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
+    
+    
+    
+    
+    @objc func chooseImage(){
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
     
     @IBAction func uploadClicked(_ sender: Any) {
         
+        let storage = Storage.storage()
+        let storageReferance = storage.reference()
+        let mediaFolder = storageReferance.child("media")
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5){
+            let imageReferance = mediaFolder.child("image.jpg")
+            imageReferance.putData(data, metadata: nil) { metadata, error in
+                if error != nil {
+                    print(error?.localizedDescription)
+                }else{
+                    imageReferance.downloadURL { url, error in
+                        if error == nil {
+                            let imageUrl = url?.absoluteString
+                            print(imageUrl)
+                        }
+                    }
+                }
+            }
+        }
     }
+    
+    
+    
     
 }
